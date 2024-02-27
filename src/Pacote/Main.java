e5c48c01f25184ebb573cd297fd7a774f0c67855
pedroryansc <pedroryangamer@gmail.com>
2024-02-27 00:51:37 -0300

    Estudo sobre o Design Pattern Singleton (23/02)

---
+++ src/Pacote/Main.java
@@ -0,0 +1,20 @@
+package Pacote;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println(Objeto.teste());
+		
+		Objeto obj1 = Objeto.getInstance();
+		Objeto obj2 = Objeto.getInstance();
+		Objeto obj3 = Objeto.getInstance();
+		
+		obj1.setCodigo(1);
+		obj2.setMsg("Teste");
+		obj3.setTipo(true);
+		
+		System.out.println(obj1);
+		System.out.println(obj2);
+		System.out.println(obj3);
+	}
+}
---
+++ src/Pacote/Objeto.java
@@ -0,0 +1,77 @@
+package Pacote;
+
+public class Objeto { // Design pattern: Singleton - Quando preciso de instância única de um objeto
+
+	// Atributos
+	
+	private static Objeto obj;
+	
+	private int codigo;
+	private String msg;
+	private boolean tipo;
+	
+	// Construtor privado
+	
+	private Objeto () {
+		
+	}
+	
+	// Métodos
+	
+	public static Objeto getInstance() {
+		if(obj == null) // Verificação para a instanciação do primeiro e único objeto
+			obj = new Objeto();
+		return obj;
+	}
+
+	public static void setObj(Objeto obj) {
+		Objeto.obj = obj;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public boolean isTipo() {
+		return tipo;
+	}
+
+	public void setTipo(boolean tipo) {
+		this.tipo = tipo;
+	}
+
+	public static String teste() {
+		return "Teste deu certo";
+	}
+	
+	// Teste à parte
+	
+	public static Objeto getObj() {
+		return obj;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Objeto [codigo=");
+		builder.append(codigo);
+		builder.append(", msg=");
+		builder.append(msg);
+		builder.append(", tipo=");
+		builder.append(tipo);
+		builder.append("]");
+		return builder.toString();
+	}
+}
